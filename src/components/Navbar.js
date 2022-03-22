9732ca424475bd5b1aaf51dd5cb329ee532fb983
Bruno RC <brunorcx@hotmail.com>
2022-03-22 16:37:43 -0400

    added less compiler config

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
+import "../styles/navbar.css";
+
 const Navbar = () => {
   return (
-    <header className="App-header ">
+    <header className="App-header">
       <nav></nav>
     </header>
   );
