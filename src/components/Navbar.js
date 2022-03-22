824953e7dcc048fce0d202093201116174cbcdee
Bruno RC <brunorcx@hotmail.com>
2022-03-22 16:11:09 -0400

    initial react app with routing

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <header className="App-header ">
+      <nav></nav>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/home.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <div>
+      Aqui está o home
+      <Navbar />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
