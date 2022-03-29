34dc2b0ad9f371cec025838824fea05492312939
brunorcx <brunorcx@hotmail.com>
2022-03-29 17:48:17 -0300

    Toggle sidebar (#2)
    * fixed PC version of products
    
    * adjusted margins for product list on mobile and tablet
    
    * toggle sidebar initial version and logic working
    
    * working toggle sidebar for mobile
    
    * initial home config for suggested products

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import "../styles/navbar.css";
 import { Link } from "react-router-dom";
-const Navbar = () => {
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
+const Navbar = ({ openSidebar, setOpenSidebar }) => {
   return (
     <header className="App-header">
       <nav className="total-nav">
@@ -8,6 +10,9 @@ const Navbar = () => {
           <img src="/Logo.png" alt="logo"></img>
         </div>
         <menu>
+          <div className="HamburgerMenu" onClick={() => setOpenSidebar(!openSidebar)}>
+            <GiHamburgerMenu />
+          </div>
           <li>
             <Link to="/">Home </Link>
           </li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,22 @@ import React from "react";
 import "../styles/home.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { useState, useEffect } from "react";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Suggested />
       <Footer />
     </div>
   );
+};
+
+const Suggested = () => {
+  const [products, setProducts] = useState(["Arroz", "Carne", "Ovos", "Frango", "Batata"]);
+  const [productsLi, setProductsLi] = useState([]);
+  return <main></main>;
 };
 
 export default Home;
