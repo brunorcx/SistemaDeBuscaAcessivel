5f50a334df50977e769fb10c0f096fbdd3cabfe0
Bruno RC <brunorcx@hotmail.com>
2022-03-29 17:06:20 -0400

    inital suggested list

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,27 @@ const Home = () => {
 const Suggested = () => {
   const [products, setProducts] = useState(["Arroz", "Carne", "Ovos", "Frango", "Batata"]);
   const [productsLi, setProductsLi] = useState([]);
-  return <main></main>;
+
+  useEffect(() => {
+    getProducts(products);
+  }, [products]);
+
+  function getProducts(products) {
+    for (const product of products) {
+      setProductsLi((productsLi) => [
+        ...productsLi,
+        <li key={product}>
+          <img src="/Arroz.jpg" alt="Arroz"></img>
+          <p>{product}</p>
+        </li>,
+      ]);
+    }
+  }
+  return (
+    <main className="suggested">
+      <ul>{productsLi}</ul>
+    </main>
+  );
 };
 
 export default Home;
